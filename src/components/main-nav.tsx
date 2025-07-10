114b92c56fc3f93eb68ba67b34ef9533c2013e10
Amaan Ahmad <asharamaan234@gmail.com>
2025-07-10 20:02:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { GalleryHorizontal, Grid3x3, Trophy, Wand2 } from "lucide-react";
 
-import { cn } from "@/lib/utils";
 import {
   SidebarMenu,
   SidebarMenuButton,
@@ -25,20 +24,18 @@ export default function MainNav() {
     <SidebarMenu>
       {navItems.map((item) => (
         <SidebarMenuItem key={item.href}>
-          <Link href={item.href} legacyBehavior passHref>
-            <SidebarMenuButton
-              asChild
-              isActive={pathname === item.href}
-              tooltip={{
-                children: item.label,
-              }}
-            >
-              <a>
-                <item.icon />
-                <span>{item.label}</span>
-              </a>
-            </SidebarMenuButton>
-          </Link>
+          <SidebarMenuButton
+            asChild
+            isActive={pathname === item.href}
+            tooltip={{
+              children: item.label,
+            }}
+          >
+            <Link href={item.href}>
+              <item.icon />
+              <span>{item.label}</span>
+            </Link>
+          </SidebarMenuButton>
         </SidebarMenuItem>
       ))}
     </SidebarMenu>
