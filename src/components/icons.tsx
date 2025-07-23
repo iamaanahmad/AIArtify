959844697ce7b123ce4350d5b4840827d98b7506
Amaan Ahmad <asharamaan234@gmail.com>
2025-07-23 12:41:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const Icons = {
   logo: (props: { className?: string }) => (
     <Image 
-      src="/Logo.png" 
+      src="https://i.ibb.co/93dZ5qdH/Art-Chain-AILogo.png" 
       alt="AIArtify Logo" 
       width={32} 
       height={32} 
