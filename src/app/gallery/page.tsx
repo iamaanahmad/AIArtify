ce69d6b51e11ac911de631d0e1f781e077cccbf6
Amaan Ahmad <asharamaan234@gmail.com>
2025-07-23 09:38:17 +0000

    How gallery, my collection and Updagrade, Support, API will work?

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,3 +1,4 @@
+
 import Image from "next/image";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
